e684fe99f15120131363e8eee5d993c5a830cea7
SaylesMand <daniel.2003qw@gmail.com>
2025-01-20 10:33:46 +0300

    edited log_name

--- scripts/ocr_attention_model.sh
+++ scripts/ocr_attention_model.sh
@@ -4,5 +4,5 @@ DATA_DIR='datasets'
 LOGS_DIR='logs'
 export PYTHONPATH=$PYTHONPATH:src
 
-python3 src/attention_model/test.py --log_dir $LOGS_DIR --log_name test_attention.txt --write_errors \
+python3 src/attention_model/test.py --log_dir $LOGS_DIR --log_name ocr_attention.txt --write_errors \
    --saved_model attention_hkr/best_cer.pth --eval_stage test --batch_size 64 --images_path datasets/img_cyrillic
